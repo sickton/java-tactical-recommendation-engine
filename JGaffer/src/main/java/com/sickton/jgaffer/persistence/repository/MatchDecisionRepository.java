90b582bd18b74c63c1e0e1150008bd36905cdb3b
Srivathsa Mantrala <svmantra@ncsu.edu>
2026-02-23 11:06:16 -0500

    Worked on api endpoint and added tactic win analytics

--- JGaffer/src/main/java/com/sickton/jgaffer/persistence/repository/MatchDecisionRepository.java
+++ JGaffer/src/main/java/com/sickton/jgaffer/persistence/repository/MatchDecisionRepository.java
@@ -1,9 +1,21 @@
 package com.sickton.jgaffer.persistence.repository;
 
 import com.sickton.jgaffer.persistence.entity.MatchDecision;
+import com.sickton.jgaffer.persistence.entity.TacticWinStats;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MatchDecisionRepository extends JpaRepository<MatchDecision, UUID> {
+    @Query("""
+SELECT 
+    m.startTactic as startTactic,
+    COUNT(m) as total,
+    SUM(CASE WHEN m.outcome = 'WIN' THEN 1 ELSE 0 END) as wins
+FROM MatchDecision m
+GROUP BY m.startTactic
+""")
+    List<TacticWinStats> getWinRateByTactic();
 }
